07733e6ac7a16e50960da00ebffd64af230e8327
Ahmad Usman <usmanah9817@gmail.com>
2020-11-23 01:29:40 +0100

    fix home page and mobile nav height

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const DOMElements = {
     navCheckbox: document.getElementById('navi-toggle'),
-    navLinks: document.querySelectorAll('.navigation__item')
+    navLinks: document.querySelectorAll('.navigation__item'),
+    introSection: document.getElementById('intro'),
+    mobileNavWrapper: document.querySelector('.navigation .wrapper')
 }
 
 const closeModal = () => {
@@ -12,3 +14,13 @@ const closeModal = () => {
 DOMElements.navLinks.forEach(navLink => {
     navLink.addEventListener('click', closeModal)
 })
+
+// Get and set Heigth of display window  as height of Intro section
+const windowHeight = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
+DOMElements.introSection.style.height = `${windowHeight}px`;
+
+// Set heigth for navigation wrapper on screen width less than 600px
+const mediaQuery = window.matchMedia("(max-width: 600px)");
+if (mediaQuery.matches) {
+    DOMElements.mobileNavWrapper.style.height = `${windowHeight}px`;
+}
